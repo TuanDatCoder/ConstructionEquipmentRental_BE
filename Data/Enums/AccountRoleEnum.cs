7a7283868503e779866ab6014841514b45f55372
TuanDatCoder <tuandatdq03@gmail.com>
2024-12-23 20:13:56 +0700

    Add Enum

---
+++ Data/Enums/AccountRoleEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildLease.Data.Models.Enums
+{
+    public enum AccountRoleEnum
+    {
+        CUSTOMER, STAFF, ADMIN, LESSOR
+    }
+}
